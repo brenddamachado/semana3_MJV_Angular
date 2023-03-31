a6d003402f8f9a4692e4ebe1e528fb6dbcee74f1
brenddamachado <brenda16machado.bm@gmail.com>
2023-03-31 20:41:28 -0300

    components

--- quarto_projeto/src/app/features/features.module.ts
+++ quarto_projeto/src/app/features/features.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListagemComponent } from './colaboradores/listagem/listagem.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListagemComponent
+  ],
   imports: [
     CommonModule
   ]
