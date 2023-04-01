ebb912feb5feb73fc55dacfd894fada03f1bb50a
brenddamachado <brenda16machado.bm@gmail.com>
2023-03-31 23:01:32 -0300

    listagem

--- quarto_projeto/src/app/features/colaboradores/listagem/listagem.component.ts
+++ quarto_projeto/src/app/features/colaboradores/listagem/listagem.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class ListagemComponent {
 
+  title: string = 'Área dos Colaboradores';
+  colab: Colabs[] = [];
+
+  constructor() {
+    this.colab = [
+      { id: 9, img: 'https://vocerh.abril.com.br/wp-content/uploads/2022/06/Eduardo_Alves.jpg?quality=70&strip=info&w=1280&h=720&crop=1', nome: 'Vitor Gabriel', cargo: 'Desenvolvedor Pl', empresa: "📚 Livraria" },
+      { id: 5, img: 'https://vocerh.abril.com.br/wp-content/uploads/2021/11/estagio-negro.jpeg?quality=70&strip=info&w=1280&h=720&crop=1', nome: 'Elisangela Machado', cargo: 'Desenvolvedora jr', empresa: "📚 Livraria" },
+      { id: 8, img: 'https://t4.ftcdn.net/jpg/04/91/98/55/360_F_491985520_6sSd5jmpm43Oo094YvosIAzgMP4vI5PX.jpg', nome: 'Bruna Lima', cargo: 'Desenvolvedora Pl', empresa: "📚 Livraria" },
+      { id: 6, img: 'https://img.freepik.com/premium-photo/confident-african-american-business-woman_33839-2893.jpg', nome: 'Brenda Machado', cargo: 'Desenvolvedora Pl', empresa: "📚 Livraria" },
+     
+    ];
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
+
+interface Colabs {
+  id: number;
+  img: any;
+  nome: string;
+  cargo: string;
+  empresa: string;
 }
