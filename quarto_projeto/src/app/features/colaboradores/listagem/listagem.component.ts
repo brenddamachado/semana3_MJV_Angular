8455bfdc6095551b75ea4048117b995caebd427c
brenddamachado <brenda16machado.bm@gmail.com>
2023-03-31 23:15:23 -0300

    Atualização final

--- quarto_projeto/src/app/features/colaboradores/listagem/listagem.component.ts
+++ quarto_projeto/src/app/features/colaboradores/listagem/listagem.component.ts
@@ -7,15 +7,15 @@ import { Component } from '@angular/core';
 })
 export class ListagemComponent {
 
-  title: string = 'Área dos Colaboradores';
+  title: string = 'Área Dos Desenvolvedores';
   colab: Colabs[] = [];
 
   constructor() {
     this.colab = [
-      { id: 9, img: 'https://vocerh.abril.com.br/wp-content/uploads/2022/06/Eduardo_Alves.jpg?quality=70&strip=info&w=1280&h=720&crop=1', nome: 'Vitor Gabriel', cargo: 'Desenvolvedor Pl', empresa: "📚 Livraria" },
-      { id: 5, img: 'https://vocerh.abril.com.br/wp-content/uploads/2021/11/estagio-negro.jpeg?quality=70&strip=info&w=1280&h=720&crop=1', nome: 'Elisangela Machado', cargo: 'Desenvolvedora jr', empresa: "📚 Livraria" },
-      { id: 8, img: 'https://t4.ftcdn.net/jpg/04/91/98/55/360_F_491985520_6sSd5jmpm43Oo094YvosIAzgMP4vI5PX.jpg', nome: 'Bruna Lima', cargo: 'Desenvolvedora Pl', empresa: "📚 Livraria" },
-      { id: 6, img: 'https://img.freepik.com/premium-photo/confident-african-american-business-woman_33839-2893.jpg', nome: 'Brenda Machado', cargo: 'Desenvolvedora Pl', empresa: "📚 Livraria" },
+      { id: 9, img: 'https://vocerh.abril.com.br/wp-content/uploads/2022/06/Eduardo_Alves.jpg?quality=70&strip=info&w=1280&h=720&crop=1', nome: 'Vitor Gabriel', cargo: 'Desenvolvedor Pleno', empresa: "📚 Livraria" },
+      { id: 5, img: 'https://vocerh.abril.com.br/wp-content/uploads/2021/11/estagio-negro.jpeg?quality=70&strip=info&w=1280&h=720&crop=1', nome: 'Elisangela Machado', cargo: 'Desenvolvedora Júnior', empresa: "📚 Livraria" },
+      { id: 8, img: 'https://f.i.uol.com.br/fotografia/2019/10/04/15702411855d97faa1e1ab3_1570241185_3x2_md.jpg', nome: 'Bruna Lima', cargo: 'Desenvolvedora Pleno', empresa: "📚 Livraria" },
+      { id: 6, img: 'https://www.napratica.org.br/wp-content/uploads/2020/07/WhatsApp-Image-2020-07-22-at-18.21.01.jpeg', nome: 'Brenda Machado', cargo: 'Desenvolvedora Sênio', empresa: "📚 Livraria" },
      
     ];
   }
